8643bb0ecfff6b20317640d2c34344553b659c72
Денис Белый <belyy-dv@yandex.ru>
2021-02-21 09:31:56 +0300

    Создание файлов

---
+++ City-mend/Create_Files.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+namespace City_mend
+{
+    class Create_Files
+    {
+        public static void Files()
+        {
+            Residents();
+            Village();
+            Food();
+            Money();
+        }
+
+        private static void Residents()
+        {
+            FileStream residents_stream = new FileStream(@"C://Сохранения игры City-mend//"+ "Save_residents.D&D", FileMode.OpenOrCreate);
+            residents_stream.Close();
+        }
+        private static void Village()
+        {
+            FileStream village_stream = new FileStream(@"C://Сохранения игры City-mend//" + "Save_villageLevel.D&D", FileMode.OpenOrCreate);
+            village_stream.Close();
+        }
+        private static void Food()
+        {
+            FileStream food_stream = new FileStream(@"C://Сохранения игры City-mend//" + "Save_food.D&D", FileMode.OpenOrCreate);
+            food_stream.Close();
+        }
+        private static void Money()
+        {
+            FileStream money_stream = new FileStream(@"C://Сохранения игры City-mend//" + "Save_money.D&D", FileMode.OpenOrCreate);
+            money_stream.Close();
+        }
+    }
+}
--- City-mend/Program.cs
+++ City-mend/Program.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace City_mend
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            
+            Directory.CreateDirectory(@"C://Сохранения игры City-mend//" + "");
+            Create_Files.Files();
+            Console.ReadLine();
         }
     }
 }
