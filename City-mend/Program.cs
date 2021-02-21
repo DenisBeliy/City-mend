bf8edb528e676495fdceff8c2e283d913bf2c5a3
Денис Белый <belyy-dv@yandex.ru>
2021-02-21 09:38:56 +0300

    FullScrean

--- City-mend/Program.cs
+++ City-mend/Program.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
 namespace City_mend
 {
     class Program
     {
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(System.IntPtr hWnd, int cmdShow);
         static void Main(string[] args)
         {
             Directory.CreateDirectory(@"C://Сохранения игры City-mend//" + "");
             Create_Files.Files();
+            FullWindow();
             Console.ReadLine();
+        }
+        
+        private static void FullWindow()
+        {
+            Process p = Process.GetCurrentProcess();
+            ShowWindow(p.MainWindowHandle, 3);
         }
     }
 }
